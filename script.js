88404fb5d0154cd02a317f418833b5d03ec87c31
Absalyamov Amir <absalyamov.amir@gmail.com>
2022-02-15 12:46:24 +0400

    test

---
+++ script.js
@@ -0,0 +1,7 @@
+const popuplinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockpadding = document.querySelectorAll(".lock-padding")
+
+let unlock = true;
+
+const timeout = 800;
