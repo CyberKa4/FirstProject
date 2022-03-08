df101fe8794e0cd1acf1a06b173adf50ad6ad7f8
Absalyamov Amir <absalyamov.amir@gmail.com>
2022-03-08 15:38:20 +0400

    FixBackground

--- script.js
+++
@@ -1,7 +0,0 @@
-const popuplinks = document.querySelectorAll('.popup-link');
-const body = document.querySelector('body');
-const lockpadding = document.querySelectorAll(".lock-padding")
-
-let unlock = true;
-
-const timeout = 800;
